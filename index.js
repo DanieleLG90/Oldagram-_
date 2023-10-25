9a1ccff37c9f18622a973be19f72ea10208d8ffb
DanieleLG90 <giacomazzi06@gmail.com>
2023-10-25 10:39:47 +0200

    adding js

--- index.js
+++ index.js
@@ -29,4 +29,15 @@ const posts = [
 ]
 
 
+let postElm = document.getElementById ("post-elm")
+let likesElm = document.getElementById ("likes-elm")
 
+likesElm.innerHTML = 21
+
+postElm.addEventListener("dblclick", function () {
+    console.log("hello")
+    let addLikes = likesElm + 1
+    
+
+    
+})
